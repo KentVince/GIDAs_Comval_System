440b3bc606fc714f885a918c80c80d5a1bc80fb2
User <User@ITCD-ITS-387>
2017-10-10 16:39:01 +0800

    4th commit indicators add

--- src/gidas_comval_system/indicators.java
+++ src/gidas_comval_system/indicators.java
@@ -38,13 +38,15 @@ public class indicators extends javax.swing.JFrame {
 
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        cbo_basic_need = new javax.swing.JComboBox<>();
         txt_desc = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         lbl_basic_id = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        cbo_basic_need = new javax.swing.JComboBox<>();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -54,6 +56,43 @@ public class indicators extends javax.swing.JFrame {
 
         jLabel3.setText("DESCRIPTION");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(28, 115, -1, -1));
+        getContentPane().add(txt_desc, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 112, 219, -1));
+
+        lbl_basic_id.setText("jLabel4");
+        getContentPane().add(lbl_basic_id, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 78, 50, -1));
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("INDICATORS");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 20, -1, -1));
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton1.setText("DELETE");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 160, 80, 40));
+
+        jButton4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton4.setText("SAVE");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, 80, 40));
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton2.setText("CANCEL");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, 80, 40));
 
         cbo_basic_need.setName(""); // NOI18N
         cbo_basic_need.addItemListener(new java.awt.event.ItemListener() {
@@ -66,65 +105,58 @@ public class indicators extends javax.swing.JFrame {
                 cbo_basic_needActionPerformed(evt);
             }
         });
-        getContentPane().add(cbo_basic_need, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 74, 219, -1));
-        getContentPane().add(txt_desc, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 112, 219, -1));
-
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton1.setText("SAVE");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(187, 180, 80, 40));
-
-        jButton2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton2.setText("CANCEL");
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 180, 80, 40));
-
-        lbl_basic_id.setText("jLabel4");
-        getContentPane().add(lbl_basic_id, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 78, 50, -1));
-
-        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setText("INDICATORS");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 20, -1, -1));
+        jPanel1.add(cbo_basic_need, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 30, 219, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 400, 240));
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton3.setText("SAVE");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(187, 180, 80, 40));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-      
-            classDb.dbconnect();
-            
-                 String timeString2="";
-                  java.util.Date d2 = new java.util.Date();
-                  SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                  timeString2 = sdf2.format(d2);
-                  String mm = timeString2;
-                  String updated = "0000-00-00 00:00:00";
-                  
+       // TODO add your handling code here:
+         classDb.dbconnect();
         
-        try {
-            s =  classDb.conn.createStatement();
-             s.executeUpdate("Insert INTO indicators(description,created_at,updated_at,basic_needs_id) "
-                + "values('" + txt_desc.getText() + "','"+ mm +"'," 
-                + "'"+ updated +"', "
-                + "'"+ lbl_basic_id.getText()+"')");
-            JOptionPane.showMessageDialog(null, "Save Record, Done.....", "Saving Record Notification", JOptionPane.INFORMATION_MESSAGE);
-
-            s.close();
-            classDb.conn.close();
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(indicators.class.getName()).log(Level.SEVERE, null, ex);
-        }
+          Object [] options = {"OK","CANCEL"};
+       if(JOptionPane.showOptionDialog(null, "Do you really want to delete this records?","Confirm!!",JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0])==1)
+       {
+     
            
-        
+       }
+       
+       else
+       {
+           
+           
+             try {
+                 s = classDb.conn.createStatement();
+                 
+                 s.executeUpdate("DELETE FROM indicators WHERE id = '" + lbl_basic_id.getText() + "' ");
+
+          
+            JOptionPane.showMessageDialog(null, "Record is Deleted, Done.....", "Delete Record Notification", JOptionPane.INFORMATION_MESSAGE);
+          
+            
+              s.close();
+                 
+                         
+              
+             } catch (SQLException ex) {
+                 Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
+             }
+            
+
+       }
+       
+           
              
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -145,6 +177,19 @@ public class indicators extends javax.swing.JFrame {
         
         
     }//GEN-LAST:event_cbo_basic_needItemStateChanged
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -186,6 +231,8 @@ public class indicators extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cbo_basic_need;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
